0cd7c221b5b6668d561f058318ea545a9a35884a
bluebay <bluebayspring@gmail.com>
2020-07-21 02:02:23 +0300

    review the lesson

--- hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
+++ hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-app.py
@@ -8,5 +8,5 @@ def hello():
 
 if __name__=='__main__':
     #app.run('localhost',port=5000, debug=True)
-    # app.run(debug=True)
-    app.run('0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run('0.0.0.0', port=80)
--- hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
+++ hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-form-handling.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # send your name as template variable, and assign route of no path ('/')
 @app.route('/')
 def home():
-    return render_template('index.html', name='Callahan')
+    return render_template('index.html', name='Spring')
 
 # Write a function named `greet` which uses template file named `greet.html` given under `templates` folder
 # and assign to the static route of ('/greet')
@@ -33,5 +33,5 @@ def login():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    # app.run(debug=True)
-    app.run('0.0.0.0', port=80)
+    app.run(debug=True)
+    #app.run('0.0.0.0', port=80)
