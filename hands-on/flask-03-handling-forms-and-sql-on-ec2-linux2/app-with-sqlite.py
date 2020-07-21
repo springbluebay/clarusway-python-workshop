2e55b2645eb86b3fe4da493c598ca924c189451e
bluebay <bluebayspring@gmail.com>
2020-07-21 13:19:07 +0300

    review flask-03

--- hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-sqlite.py
+++ hands-on/flask-03-handling-forms-and-sql-on-ec2-linux2/app-with-sqlite.py
@@ -22,7 +22,7 @@ data = """
 INSERT INTO users
 VALUES
 	("Buddy Rich", "buddy@clarusway.com" ),
-	("Candido", "candido@clarusway.com"),
+	("Spring", "spring@clarusway.com"),
 	("Charlie Byrd", "charlie.byrd@clarusway.com");
 """
 
@@ -99,5 +99,5 @@ def add_email():
 
 # Add a statement to run the Flask application which can be reached from any host on port 80.
 if __name__ == '__main__':
-    # app.run(debug=True)
+    #app.run(debug=True)
     app.run(host='0.0.0.0', port=80)
