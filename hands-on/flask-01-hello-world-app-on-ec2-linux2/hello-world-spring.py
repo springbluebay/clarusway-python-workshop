a388eea1f0deab8c94d270dc31583855bf947069
bluebay <bluebayspring@gmail.com>
2020-09-05 12:32:49 +0300

    flask-01

--- hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-spring.py
+++ hands-on/flask-01-hello-world-app-on-ec2-linux2/hello-world-spring.py
@@ -6,5 +6,5 @@ app = Flask(__name__)
 def hello():
     return 'Hello World from Spring'
 
-if __name__=='__main__':
-    app.run('0.0.0.0', port=80)
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=80)
